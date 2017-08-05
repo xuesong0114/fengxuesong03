875efd1b208f9439f2a33365169e25909ccba87d
xuesong0114 <904102304@qq.com>
2017-08-05 21:06:42 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/demo/sanci.java
@@ -0,0 +1,14 @@
+package cn.bdqn.tangcco.tcmp056.demo;
+
+import org.junit.Test;
+
+/**
+ * Created by feng0114 on 2017/8/5.
+ */
+public class sanci {
+
+    @Test
+    public void text() {
+        System.out.println("那年，那年！！！");
+    }
+}
